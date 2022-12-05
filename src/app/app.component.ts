1ff1b507c5d1f1ae40c667ddeffdad1cf4490101
sandeetukaramsa <sandeepsargar2000@gmail.com>
2022-12-05 11:19:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  
+  posts = [
+
+    {
+  
+     title: 'Neat Tree',
+  
+     imageUrl: 'assets/tree.jpeg',
+  
+     username: 'nature',
+  
+     content: 'I saw this awesome neat tree during my hike today'
+  
+    },
+  
+    {
+  
+     title: 'Snowy Mountain',
+  
+     imageUrl: 'assets/mountain.jpeg',
+  
+     username: 'mountainlover',
+  
+     content: 'Here is the picture of snowy mountain'
+  
+    },
+  
+    {
+  
+     title: 'Mountain Biking',
+  
+     imageUrl: 'assets/biking.jpeg',
+  
+     username: 'bikingbro',
+  
+     content: 'I did some biking today'
+  
+    },
+
+    {
+
+      title: 'Apna  Biking',
+  
+      imageUrl: 'assets/biking.jpeg',
+   
+      username: 'bikingbro',
+   
+      content: 'I did some biking today'
+
+    }
+    
+    
+  
+   ];
 }
